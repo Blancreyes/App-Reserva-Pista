0dce62c70752f21a0175c4fa62e116a01b7a5866
JMP160880 <javiermoranpando@gmail.com>
2023-03-12 11:43:44 +0000

    conflicto resuelto

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./pages/home";
+import { Inicio } from "./pages/inicio";
 import { Demo } from "./pages/demo";
 import { Single } from "./pages/single";
 import { Alta_usuario } from "./pages/alta_usuario";
 import injectContext from "./store/appContext";
-import { Navbar } from "./component/navbar";
+import { Navbar } from "./component/navbar.jsx";
 import { Footer } from "./component/footer";
 
 //create your first component
@@ -22,7 +22,7 @@ const Layout = () => {
         <ScrollToTop>
           <Navbar />
           <Routes>
-            <Route element={<Home />} path="/" />
+            <Route element={<Inicio />} path="/" />
             <Route element={<Demo />} path="/demo" />
             <Route element={<Single />} path="/single/:theid" />
             <Route element={<Alta_usuario />} path="/alta_usuario" />
