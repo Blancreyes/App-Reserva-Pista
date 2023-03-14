5d7cd44b57dc9fa0c61793819af4dda7db881a08
JMP160880 <javiermoranpando@gmail.com>
2023-03-14 20:09:10 +0000

    terminar datos campos

--- src/front/js/pages/alta_usuario.js
+++ src/front/js/pages/alta_usuario.js
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useContext, useState } from "react";
+import { Context } from "../store/appContext";
 import "../../styles/component.css";
+import { useNavigate } from "react-router-dom";
 
 export const Alta_usuario = () => {
+  const { store, actions } = useContext(Context);
+  const { email, setEmail } = useState();
+  const { password, setPassword } = useState();
+  const { name, setName } = useState();
+  const { lastname, setLastname } = useState();
+  const navigate = useNavigate();
+
+  async function handleAltaUsuario(e) {
+    e.preventDefault();
+    let alta = actions.altaUsuario(name, lastname, email, password);
+    if (alta) {
+      setName("");
+      setEmail("");
+      setLastname("");
+      setPassword("");
+      alert("Usuario creado correctamente");
+      navigate("/");
+    }
+  }
+
   return (
     <div className="container-lg">
       <div className="fw-bold text-center fs-2 mt-3 mb-0">DATOS USUARIO</div>
@@ -10,7 +32,13 @@ export const Alta_usuario = () => {
           <span className="input-group-text fs-5">NOMBRE</span>
         </div>
         <div className="col">
-          <input type="text" className="form-control" />
+          <input
+            type="text"
+            className="form-control"
+            id="nombre"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </div>
       </div>
       <div className="row g-4 my-4">
@@ -42,6 +70,7 @@ export const Alta_usuario = () => {
           id="boton-guardar"
           className="btn btn-success  fw-bold"
           type="button"
+          onClick={handleAltaUsuario}
         >
           GUARDAR
         </button>
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,54 +1,77 @@
-const getState = ({ getStore, getActions, setStore }) => {
-	return {
-		store: {
-			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
-		},
-		actions: {
-			// Use getActions to call a function within a fuction
-			exampleFunction: () => {
-				getActions().changeColor(0, "green");
-			},
+import axios from "axios";
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            message: null,
+            demo: [{
+                    title: "FIRST",
+                    background: "white",
+                    initial: "white",
+                },
+                {
+                    title: "SECOND",
+                    background: "white",
+                    initial: "white",
+                },
+            ],
+        },
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
 
-			getMessage: async () => {
-				try{
-					// fetching data from the backend
-					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
-					const data = await resp.json()
-					setStore({ message: data.message })
-					// don't forget to return something, that is how the async resolves
-					return data;
-				}catch(error){
-					console.log("Error loading message from backend", error)
-				}
-			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
+            getMessage: async () => {
+                try {
+                    // fetching data from the backend
+                    const resp = await fetch(process.env.BACKEND_URL + "/api/hello");
+                    const data = await resp.json();
+                    setStore({
+                        message: data.message,
+                    });
+                    // don't forget to return something, that is how the async resolves
+                    return data;
+                } catch (error) {
+                    console.log("Error loading message from backend", error);
+                }
+            },
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
 
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
 
-				//reset the global store
-				setStore({ demo: demo });
-			}
-		}
-	};
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
+            altaUsuario: async (name, lastname, email, password) => {
+                try {
+                    let response = await axios.post(
+                        "https://3000-blancreyes-appreservame-0d52zrgjgnr.ws-eu90.gitpod.io/alta_usuario", {
+                            name: name,
+                            lastname: lastname,
+                            email: email,
+                            password: password,
+                        }
+                    );
+                    return true;
+                } catch (error) {
+                    console.log(error);
+                }
+            },
+        },
+    };
 };
 
 export default getState;
