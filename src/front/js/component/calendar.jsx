4594737d8d72e9a3521d3559df3028ec52cf814c
Solracort <solracort@gmail.com>
2023-03-21 16:56:09 +0000

    calendario con botones

--- src/front/js/component/calendar.jsx
+++ src/front/js/component/calendar.jsx
@@ -1,10 +1,5 @@
 import React, { useState } from "react";
 export const Calendar = () => {
-  const [events, setEvents] = useState([
-    { title: "Meeting", start_time: "9:00am" },
-    { title: "Lunch", start_time: "12:00pm" },
-    { title: "Conference Call", start_time: "2:00pm" },
-  ]);
   const daysOfWeek = [
     "Lunes ",
     "Martes ",
@@ -40,7 +35,7 @@ export const Calendar = () => {
     currentWeek.getMonth(),
     currentWeek.getDate() - currentWeek.getDay() + 7
   );
-
+  // Esta funcion recorre el array "day" y pinta la cabecera de la tabla
   const renderHeader = () => {
     const headerCells = daysOfWeek.map((day) => {
       return (
@@ -60,35 +55,20 @@ export const Calendar = () => {
   };
   const renderRows = () => {
     const rows = hoursOfDay.map((hour, index) => {
-      const rowCells = [];
-      for (let i = 0; i < 7; i++) {
-        const eventsForDay = events.filter((e) => {
-          const startTime = new Date(
-            startOfWeek.getFullYear(),
-            startOfWeek.getMonth(),
-            startOfWeek.getDate() + i,
-            hour.split(":")[0],
-            hour.split(":")[1]
-          ).getTime();
-          return (
-            // startTime >= startOfWeek.getTime() && endTime <= endOfWeek.getTime()
-            startTime >= startOfWeek.getTime()
-          );
-        });
-        rowCells.push(
-          <td key={i} className="border border-success">
-            {eventsForDay.map((event, index) => (
-              <div key={index} className="event">
-                {event.title}
-              </div>
-            ))}
+      const dayswithhour = daysOfWeek.map((dia) => {
+        return (
+          <td className="border-success rounded-4" key={dia}>
+            <div key={dia + hour} className="rounded-4  border border-success">
+              Free Hour {dia + hour}
+            </div>
           </td>
         );
-      }
+      });
+
       return (
         <tr key={hour}>
           <td className="border border-success">{hour}</td>
-          {rowCells}
+          <td className="border border-success rounded-4">{dayswithhour}</td>
         </tr>
       );
     });
@@ -102,7 +82,7 @@ export const Calendar = () => {
       </h2>
       {/* A la hora de pintar la tabla distinguimos primero pintando la cabecera y luego el resto de filas  */}
       <table className="table table-success border border-success m-auto w-75">
-        {renderHeader()}
+        {/* {renderHeader()} */}
         {renderRows()}
       </table>
     </div>
